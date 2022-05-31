152b165fd3363afca9ce0f12d907cf74c265d25d
Prasiddha Poudyal <p.prasiddha@gmail.com>
2022-05-31 08:13:50 +0545

    More optimized

--- app.ts
+++ app.ts
@@ -1,32 +1,42 @@
 type Saved = {
-    states: Array<boolean>
     items: Array<string>
+    areCompleted: Array<boolean>
+}
+interface Item {
+    item: string
+    isCompleted: boolean
+    createList(): void
+    addNew(item: String): void
+    delete(e: Event): void
+    complete(e: Event): void
 }
 
-class Item {
-    task: string
-    state: boolean
+let msg: HTMLElement = document.getElementById('message')!
+
+class Todo implements Item {
+    item: string
+    isCompleted: boolean
     static counter: number = 0
-    constructor(task: string, state: boolean) {
-        this.task = task
-        this.state = state
+    constructor(item: string, isCompleted: boolean) {
+        this.item = item
+        this.isCompleted = isCompleted
     }
-    addNew(): void {
-        const listItem = document.createTextNode(this.task)
-        const btnDelete = document.createElement('button')
-        const btnCompleted = document.createElement('button')
+    createList(): void {
+        let listItem = document.createTextNode(this.item)
+        let btnDelete = document.createElement('button')
+        let btnCompleted = document.createElement('button')
         btnDelete.innerText = "Delete"
         btnCompleted.innerText = "Mark as completed"
         btnDelete.classList.add("btn")
         btnDelete.classList.add("btn-danger")
-        btnDelete.onclick = function (e) { markDelete(e) }
+        btnDelete.onclick = (e) => this.delete(e)
         btnCompleted.classList.add("btn")
         btnCompleted.classList.add("btn-success")
-        btnCompleted.onclick = function (e) { markCompleted(e) }
-        if (this.state) {
-            const liUp = document.createElement('li')
+        btnCompleted.onclick = (e) => this.complete(e)
+        if (!this.isCompleted) {
+            let liUp = document.createElement('li')
             liUp.classList.add("list-group-item")
-            liUp.setAttribute("id", `list_${Item.counter++}`)
+            liUp.setAttribute("id", `list_${Todo.counter++}`)
             liUp.appendChild(listItem)
             liUp.appendChild(btnDelete)
             liUp.appendChild(btnCompleted)
@@ -34,71 +44,72 @@ class Item {
             todoUL.appendChild(liUp)
         }
         else {
-            const liDown = document.createElement('li')
+            let liDown = document.createElement('li')
             liDown.classList.add("list-group-item")
-            liDown.setAttribute("id", `list_${Item.counter++}`)
+            liDown.setAttribute("id", `list_${Todo.counter++}`)
             liDown.appendChild(listItem)
             liDown.appendChild(btnDelete)
             let completedUL: HTMLElement = document.getElementById('completedUL')!
             completedUL.appendChild(liDown)
         }
     }
-    static removeAll(): void {
-        document.getElementById("todoUL")!.innerHTML = "";
-        document.getElementById("completedUL")!.innerHTML = "";
+    addNew(): void {
+        saved.items.push(this.item)
+        saved.areCompleted.push(false)
+        localStorage.setItem("items", JSON.stringify(saved.items))
+        localStorage.setItem("areCompleted", JSON.stringify(saved.areCompleted))
+        let inputForm: HTMLFormElement = <HTMLFormElement>document.getElementById('inputForm')
+        inputForm.reset()
+        Todo.reclone()
+        msg.innerText = `New todo: ${this.item} has been added.`
+    }
+    delete(e: Event): void {
+        let parentId: string = (<HTMLLIElement>(<HTMLButtonElement>e.target).parentNode).id
+        let index: number = parseInt(parentId.split('_')[1])
+        saved.items.splice(index, 1)
+        saved.areCompleted.splice(index, 1)
+        localStorage.setItem("items", JSON.stringify(saved.items))
+        localStorage.setItem("areCompleted", JSON.stringify(saved.areCompleted))
+        Todo.reclone()
+        msg.innerText = `Todo ${this.item} has been removed.`
+    }
+    complete(e: Event): void {
+        let parentId: string = (<HTMLLIElement>(<HTMLButtonElement>e.target).parentNode).id
+        let index: number = parseInt(parentId.split('_')[1])
+        saved.areCompleted.splice(index, 1, !saved.areCompleted[index])
+        localStorage.setItem("areCompleted", JSON.stringify(saved.areCompleted))
+        Todo.reclone()
+        msg.innerText = `Todo ${this.item} is marked completed.`
+    }
+    static clearAll(): void {
+        localStorage.clear()
+        saved.items = []
+        saved.areCompleted = []
+        Todo.reclone()
+        msg.innerText = `All Todo items are cleared.`
+    }
+    static reclone(): void {
+        document.getElementById("todoUL")!.innerHTML = ""
+        document.getElementById("completedUL")!.innerHTML = ""
+        Todo.counter = 0
+        saved.items.forEach((item: string, index: number) => {
+            new Todo(item, saved.areCompleted[index]).createList()
+        })
     }
 }
 
-const saved: Saved = { states: localStorage.states && JSON.parse(localStorage.states) || [], items: localStorage.items && JSON.parse(localStorage.items) || [] }
-reclone()
-let save: HTMLElement = document.getElementById('save')!
-let msg: HTMLElement = document.getElementById('message')!
-
-function reclone(): void {
-    Item.removeAll()
-    Item.counter = 0
-    saved.items.forEach((todo: string, index: number): void => {
-        new Item(todo, saved.states[index]).addNew()
-    })
-}
-
-save.onclick = function (e: Event): void {
+let saved: Saved = { items: localStorage.items && JSON.parse(localStorage.items) || [], areCompleted: localStorage.areCompleted && JSON.parse(localStorage.areCompleted) || [] }
+let addButton: HTMLElement = document.getElementById('addButton')!
+let clearButton: HTMLElement = document.getElementById('clearButton')!
+addButton.onclick = (e: Event) => {
     e.preventDefault()
     let item: string = (<HTMLInputElement>document.getElementById('todoName')!).value
-    saved.states.push(true)
-    saved.items.push(item)
-    localStorage.setItem("states", JSON.stringify(saved.states))
-    localStorage.setItem("items", JSON.stringify(saved.items))
-    new Item(item, true).addNew()
-    let inputForm: HTMLFormElement = <HTMLFormElement>document.getElementById('inputForm')
-    msg.innerText = `ToDo ${item} added successfully.`
-    inputForm.reset()
+    if (item === "") {
+        msg.innerHTML = `<div class="alert alert-danger">Cannot add empty item.</div>`
+    } else {
+        new Todo(item, false).addNew()
+    }
 }
-
-function markCompleted(e: Event): void {
-    let parentId: string = (<HTMLLIElement>(<HTMLButtonElement>e.target).parentNode).id
-    let index: number = parseInt(parentId.split('_')[1])
-    saved.states.splice(index, 1, !saved.states[index])
-    localStorage.setItem("states", JSON.stringify(saved.states))
-    reclone()
-    msg.innerText = `One Todo item moved to completed.`
-}
-
-function markDelete(e: Event): void {
-    let parentId: string = (<HTMLLIElement>(<HTMLButtonElement>e.target).parentNode).id
-    let index: number = parseInt(parentId.split('_')[1])
-    saved.states.splice(index, 1)
-    saved.items.splice(index, 1)
-    localStorage.setItem("states", JSON.stringify(saved.states))
-    localStorage.setItem("items", JSON.stringify(saved.items))
-    reclone()
-    msg.innerText = `One Todo item has been deleted.`
-}
-
-function clearAll(): void {
-    localStorage.clear()
-    saved.items = []
-    saved.states = []
-    reclone()
-    msg.innerText = `All Todo items have been cleared.`
+clearButton.onclick = () => {
+    Todo.clearAll()
 }
--- js/app.js
+++ js/app.js
@@ -1,93 +1,100 @@
 "use strict";
-var Item = /** @class */ (function () {
-    function Item(task, state) {
-        this.task = task;
-        this.state = state;
+let msg = document.getElementById('message');
+class Todo {
+    constructor(item, isCompleted) {
+        this.item = item;
+        this.isCompleted = isCompleted;
     }
-    Item.prototype.addNew = function () {
-        var listItem = document.createTextNode(this.task);
-        var btnDelete = document.createElement('button');
-        var btnCompleted = document.createElement('button');
+    createList() {
+        let listItem = document.createTextNode(this.item);
+        let btnDelete = document.createElement('button');
+        let btnCompleted = document.createElement('button');
         btnDelete.innerText = "Delete";
         btnCompleted.innerText = "Mark as completed";
         btnDelete.classList.add("btn");
         btnDelete.classList.add("btn-danger");
-        btnDelete.onclick = function (e) { markDelete(e); };
+        btnDelete.onclick = (e) => this.delete(e);
         btnCompleted.classList.add("btn");
         btnCompleted.classList.add("btn-success");
-        btnCompleted.onclick = function (e) { markCompleted(e); };
-        if (this.state) {
-            var liUp = document.createElement('li');
+        btnCompleted.onclick = (e) => this.complete(e);
+        if (!this.isCompleted) {
+            let liUp = document.createElement('li');
             liUp.classList.add("list-group-item");
-            liUp.setAttribute("id", "list_".concat(Item.counter++));
+            liUp.setAttribute("id", `list_${Todo.counter++}`);
             liUp.appendChild(listItem);
             liUp.appendChild(btnDelete);
             liUp.appendChild(btnCompleted);
-            var todoUL = document.getElementById('todoUL');
+            let todoUL = document.getElementById('todoUL');
             todoUL.appendChild(liUp);
         }
         else {
-            var liDown = document.createElement('li');
+            let liDown = document.createElement('li');
             liDown.classList.add("list-group-item");
-            liDown.setAttribute("id", "list_".concat(Item.counter++));
+            liDown.setAttribute("id", `list_${Todo.counter++}`);
             liDown.appendChild(listItem);
             liDown.appendChild(btnDelete);
-            var completedUL = document.getElementById('completedUL');
+            let completedUL = document.getElementById('completedUL');
             completedUL.appendChild(liDown);
         }
-    };
-    Item.removeAll = function () {
+    }
+    addNew() {
+        saved.items.push(this.item);
+        saved.areCompleted.push(false);
+        localStorage.setItem("items", JSON.stringify(saved.items));
+        localStorage.setItem("areCompleted", JSON.stringify(saved.areCompleted));
+        let inputForm = document.getElementById('inputForm');
+        inputForm.reset();
+        Todo.reclone();
+        msg.innerText = `New todo: ${this.item} has been added.`;
+    }
+    delete(e) {
+        let parentId = e.target.parentNode.id;
+        let index = parseInt(parentId.split('_')[1]);
+        saved.items.splice(index, 1);
+        saved.areCompleted.splice(index, 1);
+        localStorage.setItem("items", JSON.stringify(saved.items));
+        localStorage.setItem("areCompleted", JSON.stringify(saved.areCompleted));
+        Todo.reclone();
+        msg.innerText = `Todo ${this.item} has been removed.`;
+    }
+    complete(e) {
+        let parentId = e.target.parentNode.id;
+        let index = parseInt(parentId.split('_')[1]);
+        saved.areCompleted.splice(index, 1, !saved.areCompleted[index]);
+        localStorage.setItem("areCompleted", JSON.stringify(saved.areCompleted));
+        Todo.reclone();
+        msg.innerText = `Todo ${this.item} is marked completed.`;
+    }
+    static clearAll() {
+        localStorage.clear();
+        saved.items = [];
+        saved.areCompleted = [];
+        Todo.reclone();
+        msg.innerText = `All Todo items are cleared.`;
+    }
+    static reclone() {
         document.getElementById("todoUL").innerHTML = "";
         document.getElementById("completedUL").innerHTML = "";
-    };
-    Item.counter = 0;
-    return Item;
-}());
-var saved = { states: localStorage.states && JSON.parse(localStorage.states) || [], items: localStorage.items && JSON.parse(localStorage.items) || [] };
-reclone();
-var save = document.getElementById('save');
-var msg = document.getElementById('message');
-function reclone() {
-    Item.removeAll();
-    Item.counter = 0;
-    saved.items.forEach(function (todo, index) {
-        new Item(todo, saved.states[index]).addNew();
-    });
+        Todo.counter = 0;
+        saved.items.forEach((item, index) => {
+            new Todo(item, saved.areCompleted[index]).createList();
+        });
+    }
 }
-save.onclick = function (e) {
+Todo.counter = 0;
+let saved = { items: localStorage.items && JSON.parse(localStorage.items) || [], areCompleted: localStorage.areCompleted && JSON.parse(localStorage.areCompleted) || [] };
+let addButton = document.getElementById('addButton');
+let clearButton = document.getElementById('clearButton');
+addButton.onclick = (e) => {
     e.preventDefault();
-    var item = document.getElementById('todoName').value;
-    saved.states.push(true);
-    saved.items.push(item);
-    localStorage.setItem("states", JSON.stringify(saved.states));
-    localStorage.setItem("items", JSON.stringify(saved.items));
-    new Item(item, true).addNew();
-    var inputForm = document.getElementById('inputForm');
-    msg.innerText = "ToDo ".concat(item, " added successfully.");
-    inputForm.reset();
+    let item = document.getElementById('todoName').value;
+    if (item === "") {
+        msg.innerHTML = `<div class="alert alert-danger">Cannot add empty item.</div>`;
+    }
+    else {
+        new Todo(item, false).addNew();
+    }
 };
-function markCompleted(e) {
-    var parentId = e.target.parentNode.id;
-    var index = parseInt(parentId.split('_')[1]);
-    saved.states.splice(index, 1, !saved.states[index]);
-    localStorage.setItem("states", JSON.stringify(saved.states));
-    reclone();
-    msg.innerText = "One Todo item moved to completed.";
-}
-function markDelete(e) {
-    var parentId = e.target.parentNode.id;
-    var index = parseInt(parentId.split('_')[1]);
-    saved.states.splice(index, 1);
-    saved.items.splice(index, 1);
-    localStorage.setItem("states", JSON.stringify(saved.states));
-    localStorage.setItem("items", JSON.stringify(saved.items));
-    reclone();
-    msg.innerText = "One Todo item has been deleted.";
-}
-function clearAll() {
-    localStorage.clear();
-    saved.items = [];
-    saved.states = [];
-    reclone();
-    msg.innerText = "All Todo items have been cleared.";
-}
+clearButton.onclick = () => {
+    Todo.clearAll();
+};
